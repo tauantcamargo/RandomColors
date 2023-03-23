1f394d1a89eacf80158f4294fe4541e6f388380d
tauantcamargo <lukyhenson@gmail.com>
2023-03-23 03:38:26 -0300

    feat: adding func to generate random colors and pass to table view cell

--- RandomColors/ColorsTableVC.swift
+++ RandomColors/ColorsTableVC.swift
@@ -8,19 +8,40 @@
 import UIKit
 
 class ColorsTableVC: UIViewController {
+    var colors: [UIColor] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        addRandomColors()
+    }
+    
+    func addRandomColors() {
+        for _ in 0..<50 {
+            colors.append(createRandomColor())
+        }
+    }
+    
+    func createRandomColor() -> UIColor {
+        let randomColor = UIColor(red: CGFloat.random(in: 0...1),
+                                  green: CGFloat.random(in: 0...1),
+                                  blue: CGFloat.random(in: 0...1),
+                                  alpha: 1)
+        return randomColor
     }
 }
 
 
 extension ColorsTableVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50
+        return colors.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell") else {
+            return UITableViewCell()
+        }
+        cell.backgroundColor = colors[indexPath.row]
+        return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
