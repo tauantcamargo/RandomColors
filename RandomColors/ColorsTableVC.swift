9f92b7c5a8289b931b3a512665750aed9e30828f
tauantcamargo <lukyhenson@gmail.com>
2023-03-23 03:18:37 -0300

    feat: creating extension to table view and table view data source

--- RandomColors/ColorsTableVC.swift
+++ RandomColors/ColorsTableVC.swift
@@ -10,11 +10,20 @@ import UIKit
 class ColorsTableVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
 
-        // Do any additional setup after loading the view.
+
+extension ColorsTableVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
     }
     
-    @IBAction func tempButtonTapped(_ sender: UIButton) {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "ToColorDetail", sender: nil)
     }
 }
