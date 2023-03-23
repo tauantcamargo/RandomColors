8882b94347ffdbddb133d8ccdf948b044f54dbfc
tauantcamargo <lukyhenson@gmail.com>
2023-03-23 04:00:56 -0300

    feat: adding enums to segues and cells reference name and creating a file to uicolor extension

--- RandomColors/ColorsTableVC.swift
+++ RandomColors/ColorsTableVC.swift
@@ -10,6 +10,14 @@ import UIKit
 class ColorsTableVC: UIViewController {
     var colors: [UIColor] = []
     
+    enum Cells {
+        static let ColorCell = "ColorCell"
+    }
+    
+    enum Segues {
+        static let ToColorDetail = "ToColorDetail"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         addRandomColors()
@@ -17,16 +25,8 @@ class ColorsTableVC: UIViewController {
     
     func addRandomColors() {
         for _ in 0..<50 {
-            colors.append(createRandomColor())
+            colors.append(.random())
         }
-    }
-    
-    func createRandomColor() -> UIColor {
-        let randomColor = UIColor(red: CGFloat.random(in: 0...1),
-                                  green: CGFloat.random(in: 0...1),
-                                  blue: CGFloat.random(in: 0...1),
-                                  alpha: 1)
-        return randomColor
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -42,7 +42,7 @@ extension ColorsTableVC: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell") else {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: Cells.ColorCell) else {
             return UITableViewCell()
         }
         cell.backgroundColor = colors[indexPath.row]
@@ -51,6 +51,6 @@ extension ColorsTableVC: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let color = colors[indexPath.row]
-        performSegue(withIdentifier: "ToColorDetail", sender: color)
+        performSegue(withIdentifier: Segues.ToColorDetail, sender: color)
     }
 }
